2f2e963ec330b10c5422de725470c8340fedb01f
Zeynep Tunayabakan <161473860+zeyneptunayabakan@users.noreply.github.com>
2024-03-21 08:38:50 +0300

    Delete lib directory

--- lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:quiz_app/homepage.dart';
-void main() {
-  runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Homepage()));
-}
